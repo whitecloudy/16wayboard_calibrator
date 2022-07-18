5ed369c3ffb3562be77b4ea154d81628d33f7aac
JaeMin Shin <alex9395@naver.com>
2022-07-18 16:28:32 +0900

    Update Main

---
+++ main.py
@@ -0,0 +1,35 @@
+from audioop import avg
+import re
+from vna.anritsu import anritsu
+from AD5371_comm import AD5371_comm, complex_packer
+import struct
+import numpy
+import cmath
+
+def main():
+    anal = anritsu("127.0.0.1", 0.910, 0.910, 1)
+    comm = AD5371_comm("192.168.1.160")
+    avg_count = 5
+
+    while(True):
+        respond_bytes = comm.recv_data()
+        respond = struct.unpack('B', respond_bytes)[0]
+        print(respond)
+        if respond == 1:
+            print("We are DONE!")
+            break
+        elif respond != 0:
+            assert False, str(respond)+": Respond from 16wayboard error"
+
+        result = 0.0
+        for i in range(avg_count):
+            anal.doSweep()
+            result += complex(anal.getTrace(2))
+        result /= avg_count
+        packed_data = complex_packer(result)
+
+        comm.send_data(packed_data)
+        
+
+if __name__=="__main__":
+    main()
